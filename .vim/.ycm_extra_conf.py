aec608be55151fe5c824e89b57a812fcff45a597
JDevlieghere <info@jonasdevlieghere.com>
2015-07-26 17:34:34 +0200

    Cleanup

--- .vim/.ycm_extra_conf.py
+++ .vim/.ycm_extra_conf.py
@@ -4,25 +4,51 @@ import fnmatch
 import logging
 import ycm_core
 
-base_flags = [
-        '-Wall',
-        '-Wextra',
-        '-Werror',
-        '-Wc++98-compat',
-        '-Wno-long-long',
-        '-Wno-variadic-macros',
-        '-fexceptions',
-        '-DNDEBUG',
-        '-std=c++98',
-        '-xc++',
-        '-I/usr/include/'
-        ]
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
-HEADER_EXTENSIONS = [ '.h', '.hxx', '.hpp', '.hh' ]
+BASE_FLAGS = [
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wc++98-compat',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-DNDEBUG',
+    '-std=c++98',
+    '-xc++',
+    '-I/usr/include/'
+]
+
+SOURCE_EXTENSIONS = [
+    '.cpp',
+    '.cxx',
+    '.cc',
+    '.c',
+    '.m',
+    '.mm'
+]
+
+HEADER_EXTENSIONS = [
+    '.h',
+    '.hxx',
+    '.hpp',
+    '.hh'
+]
 
 def IsHeaderFile(filename):
     extension = os.path.splitext(filename)[1]
     return extension in HEADER_EXTENSIONS
+
+def GetCompilationInfoForFile(database, filename):
+    if IsHeaderFile(filename):
+        basename = os.path.splitext(filename)[0]
+        for extension in SOURCE_EXTENSIONS:
+            replacement_file = basename + extension
+            if os.path.exists(replacement_file):
+                compilation_info = database.GetCompilationInfoForFile(replacement_file)
+                if compilation_info.compiler_flags_:
+                    return compilation_info
+        return None
+    return database.GetCompilationInfoForFile(filename)
 
 def FindNearest(path, target):
     candidate = os.path.join(path, target)
@@ -32,7 +58,6 @@ def FindNearest(path, target):
     else:
         parent = os.path.dirname(os.path.abspath(path));
         if(parent == path):
-            logging.info("Unable to find " + target);
             raise RuntimeError("Could not find " + target);
         return FindNearest(parent, target)
 
@@ -47,21 +72,21 @@ def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
 
         if make_next_absolute:
             make_next_absolute = False
-            if not flag.startswith( '/' ):
-                new_flag = os.path.join( working_directory, flag )
+            if not flag.startswith('/'):
+                new_flag = os.path.join(working_directory, flag)
 
         for path_flag in path_flags:
             if flag == path_flag:
                 make_next_absolute = True
                 break
 
-            if flag.startswith( path_flag ):
-                path = flag[ len( path_flag ): ]
-                new_flag = path_flag + os.path.join( working_directory, path )
+            if flag.startswith(path_flag):
+                path = flag[ len(path_flag): ]
+                new_flag = path_flag + os.path.join(working_directory, path)
                 break
 
         if new_flag:
-            new_flags.append( new_flag )
+            new_flags.append(new_flag)
     return new_flags
 
 
@@ -85,14 +110,14 @@ def FlagsForInclude(root):
     except:
         return None
 
-def GetCompilationDatabaseFlags(root, filename):
+def FlagsForCompilationDatabase(root, filename):
     try:
         compilation_db_path = FindNearest(root, 'compile_commands.json')
         compilation_db_dir = os.path.dirname(compilation_db_path)
         logging.info("Set compilation database directory to " + compilation_db_dir)
         compilation_db =  ycm_core.CompilationDatabase(compilation_db_dir)
         if not compilation_db:
-            logging.info("Found compilation database file but unable to load database")
+            logging.info("Compilation database file found but unable to load")
             return None
         compilation_info = GetCompilationInfoForFile(compilation_db, filename)
         if not compilation_info:
@@ -101,36 +126,22 @@ def GetCompilationDatabaseFlags(root, filename):
         return MakeRelativePathsInFlagsAbsolute(
             compilation_info.compiler_flags_,
             compilation_info.compiler_working_dir_)
-    except Exception as e:
-        logging.warn(e)
+    except:
         return None
-
-def GetCompilationInfoForFile(database, filename):
-    if IsHeaderFile(filename):
-        basename = os.path.splitext(filename)[0]
-        for extension in SOURCE_EXTENSIONS:
-            replacement_file = basename + extension
-            if os.path.exists(replacement_file):
-                compilation_info = database.GetCompilationInfoForFile(replacement_file)
-                if compilation_info.compiler_flags_:
-                    return compilation_info
-        return None
-    return database.GetCompilationInfoForFile(filename)
 
 def FlagsForFile(filename):
     root = os.path.realpath(filename);
-    compilation_db_flags = GetCompilationDatabaseFlags(root, filename)
+    compilation_db_flags = FlagsForCompilationDatabase(root, filename)
     if compilation_db_flags:
         final_flags = compilation_db_flags
     else:
-        final_flags = base_flags
+        final_flags = BASE_FLAGS
         clang_flags = FlagsForClangComplete(root)
-        include_flags = FlagsForInclude(root)
         if clang_flags:
             final_flags = final_flags + clang_flags
+        include_flags = FlagsForInclude(root)
         if include_flags:
             final_flags = final_flags + include_flags
-    logging.info(final_flags)
     return {
         'flags': final_flags,
         'do_cache': True
--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-Dotfiles
-========
+Dotfiles ========
 
 This is my dotfile repository. There are many like it, but this one is mine.
 
- - [Installation](#installation)
- - [Vim](#vim)
- - [Xmonad](#xmonad)
+ - [Installation](#installation) [Vim](#vim) [Xmonad](#xmonad)
 
 ## Installation
 
-With `git` and `curl` installed, run the command below from the home directory. This will clone the dotfiles repository and run the bootstrap script.
+With `git` and `curl` installed, run the command below from the home directory.
+This will clone the dotfiles repository and run the bootstrap script.
 
 ```
 git clone https://github.com/JDevlieghere/dotfiles.git ~/dotfiles && source ~/dotfiles/bootstrap.sh
@@ -17,14 +15,30 @@ git clone https://github.com/JDevlieghere/dotfiles.git ~/dotfiles && source ~/do
 
 ## Vim
 
-Vim is my editor of choice and I've tweaked my configuration quite a bit to make it fit my particular needs. Today it's used mainly for C++ development. I use [Vundle](https://github.com/VundleVim/Vundle.vim) to manage my plugins. You can have a look at the respective repositories to discover what each of them does. 
+Vim is my editor of choice and I've tweaked my configuration quite a bit to
+make it fit my particular needs. Today it's used mainly for C++ development. I
+use [Vundle](https://github.com/VundleVim/Vundle.vim) to manage my plugins. You
+can have a look at the respective repositories to discover what each of them
+does.
 
 ### YouCompleteMe
 
-One plugin worth mentioning is [YouCompleteMe](https://github.com/Valloric/YouCompleteMe) as it requires some attention to install, so make sure to check its documentation. 
+One plugin worth mentioning is
+[YouCompleteMe](https://github.com/Valloric/YouCompleteMe) as it requires some
+attention to install, so make sure to check its documentation.
 
-For semantic completion it [requires](https://github.com/Valloric/YouCompleteMe#c-family-semantic-completion-engine-usage) an addition configuration file `.ycm_extra_conf.py` containing project specific compilation flags. I use a global file that looks for a `include` directory and a `.clang_complete` file in the current working directory or in any directory above it in the hierarchy. It then reads the compile flags from the `.clang_complete` file and adds `-I` flags all subdirectories of the nearest `include` folder. 
+For semantic completion it
+[requires](https://github.com/Valloric/YouCompleteMe#c-family-semantic-completion-engine-usage)
+an addition configuration file `.ycm_extra_conf.py` containing project specific
+compilation flags. I use a global file that looks for a [compilation
+database](http://clang.llvm.org/docs/JSONCompilationDatabase.html) or
+alternatively for an `include` directory and a `.clang_complete` file in the
+current working directory or in any directory above it in the hierarchy. It
+then reads the compile flags from the respective file.
 
 ## Xmonad
 
-When I got started with xmonad I came across [Vic Fryzel's configuration](https://github.com/vicfryzel/xmonad-config) which I grew fond of over time.  It is still largely the same, except for a few small changes here and there. 
+When I got started with xmonad I came across [Vic Fryzel's
+configuration](https://github.com/vicfryzel/xmonad-config) which I grew fond of
+over time.  It is still largely the same, except for a few small changes here
+and there.
