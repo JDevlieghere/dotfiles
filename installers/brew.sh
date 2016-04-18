16c879f146c683440ba352ae4ef409403af81738
Jonas Devlieghere <jonas@devlieghere.com>
2016-04-19 00:05:53 +0200

    Easy install

---
+++ installers/brew.sh
@@ -0,0 +1,33 @@
+# Install Homebrew or make sure it's up to date
+which -s brew
+if [[ $? != 0 ]] ; then
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+	brew upgrade --all
+fi
+
+# Install formulae
+brew install binutils
+brew install cmake
+brew install coreutils
+brew install findutils
+brew install gcc --without-multilib
+brew install git
+brew install go
+brew install graphviz
+brew install htop
+brew install lua
+brew install macvim --with-override-system-vim
+brew install neovim/neovim/neovim
+brew install node
+brew install python
+brew install rust
+brew install tmux
+brew install weechat --with-perl --with-python
+brew install wget --with-iri
+brew install xz
+brew install zsh
+
+# Cleanup
+brew cleanup
---
+++ installers/python.sh
@@ -0,0 +1,6 @@
+# Make sure we have the last version
+pip install --upgrade pip
+
+# Install packages
+pip install gprof2dot
+pip install haxor-news
