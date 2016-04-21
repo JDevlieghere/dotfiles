5de045181f9f5eb25ff4b0f6d9536817ebe9c896
Jonas Devlieghere <jdevlieghere@home.jonasdevlieghere.com>
2016-04-21 18:13:40 +0200

    Added apt install script

---
+++ installers/apt.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+sudo apt-get install -y curl
+sudo apt-get install -y exuberant-ctags
+sudo apt-get install -y git
+sudo apt-get install -y htop
+sudo apt-get install -y ncurses
+sudo apt-get install -y nodejs
+sudo apt-get install -y scrot
+sudo apt-get install -y tmux
+sudo apt-get install -y vim
+sudo apt-get install -y weechat-curses weechat-plugins
+sudo apt-get install -y zsh
+
