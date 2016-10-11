0f22859a79a30afee36014d02caa5a5034f3f8fe
Jonas Devlieghere <jonas@devlieghere.com>
2016-10-11 11:04:00 +0200

    Use Tmux Plugin Manager

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,6 +37,7 @@ doInstall() {
     info "Installing Extras"
     curl -sL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
     if [ "$(uname)" == "Darwin" ]; then
