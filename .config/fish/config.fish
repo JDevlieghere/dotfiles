43505d128b9aa8102c328e809261ebe0523a20c0
Jonas Devlieghere <jonas@devlieghere.com>
2016-11-03 09:31:11 +0100

    Start GPG agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,12 @@
-# No greeting
+# No greeting.
 set --erase fish_greeting
 
+# Source dotfiles.
 source ~/.aliases
 source ~/.exports
 
+# Source fish files.
+source ~/.config/fish/gpg.fish
+
+# Source localrc which is not sync'd.
 source ~/.localrc
