e90ce31394a60771fbf9b29257a20e0cbe31903a
Jonas Devlieghere <jonas@devlieghere.com>
2016-11-05 01:06:53 +0100

    Fix GPG agent env file

--- .config/fish/gpg.fish
+++ .config/fish/gpg.fish
@@ -6,7 +6,7 @@ function __refresh_gpg_agent_info -d "Re-load ~/.gpg-agent-info into environment
 end
 
 if not set -q -x GPG_AGENT_INFO
-	gpg-agent --daemon >/dev/null
+	gpg-agent --daemon --write-env-file ~/.gpg-agent-info >/dev/null
 end
 
 if test -f ~/.gpg-agent-info
@@ -15,7 +15,7 @@ if test -f ~/.gpg-agent-info
 	gpg-connect-agent /bye ^/dev/null
 	if test $status -eq 1
 		pkill -U $USER gpg-agent
-		gpg-agent --daemon >/dev/null
+		gpg-agent --daemon --write-env-file ~/.gpg-agent-info >/dev/null
 		__refresh_gpg_agent_info
 	end
 end
