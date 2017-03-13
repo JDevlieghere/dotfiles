8d00212c171c9f56a044078d260f352504c7d278
Jonas Devlieghere <jonas@devlieghere.com>
2017-03-13 19:56:11 +0100

    Improve llvm mono script

--- installers/llvm-mono.sh
+++ installers/llvm-mono.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
 # Configures LLVM to be build and installed in the current directory.
+# http://llvm.org/docs/GettingStarted.html#for-developers-to-work-with-a-git-monorepo
+#
+# Usage: ./llvm-mono.sh [projects] [cmake build type]
+#
+# The script will create following directory structure:
 # .
 # ├── build
 # ├── install
-# └── llvm
+# └── llvm-project
 
-# http://llvm.org/docs/GettingStarted.html#for-developers-to-work-with-a-git-monorepo
+# CMake Build Type
+if [ -z "$1" ]; then
+    PROJECTS="clang;clang-tools-extra"
+else
+    PROJECTS="$1"
+fi
 
 TOP_LEVEL_DIR=$(pwd)
+
 export TOP_LEVEL_DIR="$TOP_LEVEL_DIR"
 export PATH=$PATH:$TOP_LEVEL_DIR/llvm-project/llvm/utils/git-svn/
 
@@ -26,7 +37,7 @@ mkdir -p build || exit 1
 cd build || exit 1
 cmake ../llvm-project/llvm \
     -G Ninja \
-    -DLLVM_ENABLE_PROJECTS="$*" \
+    -DLLVM_ENABLE_PROJECTS="$PROJECTS" \
     -DCMAKE_INSTALL_PREFIX="$TOP_LEVEL_DIR/install" \
     -DCMAKE_BUILD_TYPE="Debug" \
     -DBUILD_SHARED_LIBS=ON
