cf8dbdce090b4141d44f957ace4b014459e891f1
Jonas Devlieghere <jonas@devlieghere.com>
2017-10-13 17:12:07 +0100

    Just print cmake command for llvm

--- .config/fish/functions/cmake-llvm.fish
+++ .config/fish/functions/cmake-llvm.fish
@@ -1,36 +1,12 @@
 # Personal presets for quickly configuring LLVM with CMake.
 function cmake-llvm -d "Configure CMake for LLVM"
-    switch $argv[1]
-    case x86rel
-        cmake "$argv[2]" \
-            -G Ninja \
-            -DLLVM_TARGETS_TO_BUILD="X86" \
-            -DCMAKE_INSTALL_PREFIX="../install" \
-            -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
-            -DBUILD_SHARED_LIBS=On \
-            -DLLVM_ENABLE_ASSERTIONS=On
-    case x86deb
-        cmake $argv[2] \
-            -G Ninja \
-            -DLLVM_TARGETS_TO_BUILD="X86" \
-            -DCMAKE_INSTALL_PREFIX="../install" \
-            -DCMAKE_BUILD_TYPE="Debug" \
-            -DBUILD_SHARED_LIBS=On
-    case rel
-        cmake $argv[2] \
-            -G Ninja \
-            -DCMAKE_INSTALL_PREFIX="../install" \
-            -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
-            -DBUILD_SHARED_LIBS=On \
-            -DLLVM_ENABLE_ASSERTIONS=On
-    case deb
-        cmake $argv[2] \
-            -G Ninja \
-            -DCMAKE_INSTALL_PREFIX="../install" \
-            -DCMAKE_BUILD_TYPE="Debug" \
-            -DBUILD_SHARED_LIBS=On
-    case '*'
-        echo "Unknown preset!"
-    end
-    ninja
+    echo "cmake $argv[1] \\"
+    echo "  -G Ninja \\"
+    echo "  -DBUILD_SHARED_LIBS=On \\"
+    echo "  -DCMAKE_BUILD_TYPE='RelWithDebInfo' \\"
+    echo "  -DCMAKE_INSTALL_PREFIX='../install'\\"
+    echo "  -DLLVM_ENABLE_ASSERTIONS=On \\"
+    echo "  -DLLVM_ENABLE_MODULES=On \\"
+    echo "  -DLLVM_TARGETS_TO_BUILD='X86' \\"
+    echo "  -DLLVM_USE_SANITIZER='Address'"
 end
