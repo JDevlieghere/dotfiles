e0beb212429d96179a5616cf3bc76baf5f82d0c7
Jonas Devlieghere <jonas@devlieghere.com>
2017-10-31 11:44:33 +0000

    Time git/cmake as a whole

--- scripts/update-llvm.sh
+++ scripts/update-llvm.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
-# Automatically updates and rebuilds LLVM. Particularly convenient for setting
-# a cron job. The first argument is the root where we should start looking for
+# Automatically updates and rebuilds LLVM. It can be particularly convenient
+# for setting up a cron job.
+#
+# The first argument is the root where we should start looking for
 # repositories and build folders.
 
 ROOT="$1"
 cd "$ROOT"
 
-for i in $(find . -type d -name ".git"); do
-    cd $(dirname "$i")
-    git pull --rebase
-    cd "$ROOT"
-done
+time {
+    for d in $(find . -type d -name ".git"); do
+        cd $(dirname "$d")
+        git pull --rebase
+        git rebase master || git rebase --abort
+        cd "$ROOT"
+    done
+}
 
-for i in $(find . -type f -name "CMakeCache.txt"); do
-    cd $(dirname "$i")
-    time cmake --build .
-    cd "$ROOT"
-done
+time {
+    for d in $(find . -type f -name "CMakeCache.txt"); do
+        cd $(dirname "$d")
+        cmake --build .
+        cd "$ROOT"
+    done
+}
