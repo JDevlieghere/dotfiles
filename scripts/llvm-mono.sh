7c8f9dc99c7e5db0198e7315691011554a9b8763
Jonas Devlieghere <jonas@devlieghere.com>
2017-11-21 20:11:49 +0000

    Cleanup script

--- scripts/llvm-mono.sh
+++ scripts/llvm-mono.sh
@@ -7,37 +7,31 @@
 #
 # The script will create following directory structure:
 # .
-# ├── build
-# ├── install
+# ├── llvm-build
+# ├── llvm-install
 # └── llvm-project
 
-# CMake Build Type
 if [ -z "$1" ]; then
     PROJECTS="clang;clang-tools-extra;compiler-rt"
 else
     PROJECTS="$1"
 fi
 
-TOP_LEVEL_DIR=$(pwd)
-
-export TOP_LEVEL_DIR="$TOP_LEVEL_DIR"
+export TOP_LEVEL_DIR=`pwd`
 export PATH=$PATH:$TOP_LEVEL_DIR/llvm-project/llvm/utils/git-svn/
 
-# Clone momorepo
 git clone https://github.com/llvm-project/llvm-project-20170507/ llvm-project
 cd llvm-project || exit 1
 git config branch.master.rebase true
 
-# Create folders
 cd "$TOP_LEVEL_DIR" || exit 1
-mkdir -p install || exit 1
-mkdir -p build || exit 1
+mkdir -p llvm-install || exit 1
+mkdir -p llvm-build || exit 1
 
-# Run CMake
-cd build || exit 1
+cd llvm-build || exit 1
 cmake ../llvm-project/llvm \
     -G Ninja \
     -DLLVM_ENABLE_PROJECTS="$PROJECTS" \
-    -DCMAKE_INSTALL_PREFIX="$TOP_LEVEL_DIR/install" \
+    -DCMAKE_INSTALL_PREFIX="$TOP_LEVEL_DIR/llvm-install" \
     -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
     -DBUILD_SHARED_LIBS=ON
