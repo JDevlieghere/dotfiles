fcb76c0425e08a245a580e255d96e95aecc6719c
Jonas Devlieghere <jonas@devlieghere.com>
2018-01-30 11:55:17 +0000

    Fix typo in cmake args

--- scripts/llvm-cmake.py
+++ scripts/llvm-cmake.py
@@ -51,7 +51,7 @@ if args.ra:
 
 if args.debug:
     cmake_cmd.append("-DCMAKE_BUILD_TYPE='Debug'")
-    cmake_cmd.append("-LLVM_OPTIMIZED_TABLEGEN:BOOL=ON")
+    cmake_cmd.append("-DLLVM_OPTIMIZED_TABLEGEN:BOOL=ON")
 
 if args.modules:
     cmake_cmd.append("-DLLVM_ENABLE_MODULES:BOOL=ON")
