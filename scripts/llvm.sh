a52b06e5a10be2be07488078aee9d9ea585fe794
Jonas Devlieghere <jonas@devlieghere.com>
2018-02-05 18:05:25 +0000

    Add LLDB

--- scripts/llvm.sh
+++ scripts/llvm.sh
@@ -24,6 +24,7 @@ function init {
     git svn rebase -l > /dev/null
 }
 
+
 # llvm
 git clone https://llvm.org/git/llvm.git
 init "$ROOT/llvm" "llvm"
@@ -38,39 +39,25 @@ cd "$ROOT/llvm/tools/clang/tools" || exit
 git clone https://llvm.org/git/clang-tools-extra.git extra
 init "$ROOT/llvm/tools/clang/tools/extra" "clang-tools-extra"
 
+# lldb
+cd "$ROOT/llvm/tools" || exit
+git clone https://llvm.org/git/lldb.git
+init "$ROOT/llvm/tools/lldb" "lldb"
+
 # compiler-rt
 cd "$ROOT/llvm/projects" || exit
 git clone https://llvm.org/git/compiler-rt.git
 init "$ROOT/llvm/projects/compiler-rt" "compiler-rt"
 
-# libcxx
-if [[ -n "$LIBCXX" ]]; then
-    cd "$ROOT/llvm/projects" || exit
-    git clone http://llvm.org/git/libcxx.git
-    git clone http://llvm.org/git/libcxxabi.git
-fi
+cd "$ROOT/llvm/projects" || exit
+git clone http://llvm.org/git/libcxx.git
+git clone http://llvm.org/git/libcxxabi.git
 
 # test-suite
-if [[ -n "$TESTSUITE" ]]; then
-    cd "$ROOT/llvm/projects" || exit
-    git clone http://llvm.org/git/test-suite.git
-fi
+cd "$ROOT/llvm/projects" || exit
+git clone http://llvm.org/git/test-suite.git
 
 # Create build and install dirs
 cd "$ROOT" || exit
 mkdir -p build
 mkdir -p install
-
-# Run Cmake
-cd build || exit
-cmake ../llvm \
-    -G Ninja \
-    -DCMAKE_INSTALL_PREFIX="../install" \
-    -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
-    -DBUILD_SHARED_LIBS=On \
-    -DCOMPILER_RT_DEBUG=On \
-    -DLLVM_INCLUDE_TESTS=On \
-    -DLLVM_ENABLE_ASSERTIONS=On
-
-# Run Ninja
-ninja
