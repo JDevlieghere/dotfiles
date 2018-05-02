2310456efd572ad7f8ba9265060756c2fc0ab871
Jonas Devlieghere <jonas@devlieghere.com>
2018-05-02 14:55:36 +0100

    Small tweak to swift script

--- scripts/swift.sh
+++ scripts/swift.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-# Builds swift in the current directory.
+# Clones swift in the current directory.
 # .
 # ├── build
 # ├── clang
 # ├── cmark
 # ├── llvm
 # └── swift
-#
-# Useful arguments to the build script include:
-#   -i (build for iOS)
-#   -r (build release with debug info)
-#   -t (test after building)
-#   --skip-test-cmark (skip cmark tests)
-#   --skip-test-swift (skip swift tests)
 
 export SWIFT_SOURCE_ROOT=$(pwd)
 export SWIFT_BUILD_ROOT=$(pwd)/build
@@ -28,10 +21,8 @@ info() {
   printf "\033[00;34m$@\033[0m\n"
 }
 
-# Start in the root.
 cd "$SWIFT_SOURCE_ROOT" || exit
 
-# Clone if directories don't exist.
 info "Cloning repositories"
 git clone git@github.com:apple/swift-clang.git "$SWIFT_CLANG_ROOT"
 git clone git@github.com:apple/swift-cmark.git "$SWIFT_CMARK_ROOT"
@@ -39,9 +30,8 @@ git clone git@github.com:apple/swift-lldb.git "$SWIFT_LLDB_ROOT"
 git clone git@github.com:apple/swift-llvm.git "$SWIFT_LLVM_ROOT"
 git clone git@github.com:apple/swift.git "$SWIFT_SWIFT_ROOT"
 
-# Update checkout
+info "Updating checkout"
 "$SWIFT_SWIFT_ROOT/utils/update-checkout" --scheme=master --reset-to-remote
 
-# Finally invoke build-script and forward all arguments.
 info "Invoking build script"
 "$SWIFT_SWIFT_ROOT/utils/build-script" "$@"
