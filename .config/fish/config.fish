dc3174182754b8da51bd9e6cbe4a33f45c5debbc
Jonas Devlieghere <jonas@devlieghere.com>
2018-05-14 12:02:31 +0100

    Revert "Attach to tmux when opening shell"
    This reverts commit bb76b33741fb97db7017fc7cb53beca79e565e19.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,6 @@ source ~/.exports
 source ~/.config/fish/gpg.fish
 source ~/.config/fish/fzf.fish
 source ~/.config/fish/ssh.fish
-source ~/.config/fish/tmux.fish
 
 # Source localrc which is not sync'd.
 source ~/.localrc
--- .config/fish/tmux.fish
+++
@@ -1,4 +0,0 @@
-if status is-interactive
-and not set -q TMUX
-    tmux attach ;or tmux new
-end
