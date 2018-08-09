e50315a5612b67aa26b5c934d695dc08c5b21896
Jonas Devlieghere <jonas@devlieghere.com>
2018-08-09 16:34:54 +0100

    Add more repos to swift script

--- scripts/swift.sh
+++ scripts/swift.sh
@@ -16,6 +16,7 @@ SWIFT_CMARK_ROOT=$SWIFT_SOURCE_ROOT/cmark
 SWIFT_LLDB_ROOT=$SWIFT_SOURCE_ROOT/lldb
 SWIFT_LLVM_ROOT=$SWIFT_SOURCE_ROOT/llvm
 SWIFT_SWIFT_ROOT=$SWIFT_SOURCE_ROOT/swift
+SWIFT_SWIFTPM_ROOT=$SWIFT_SOURCE_ROOT/swiftpm
 
 info() {
   printf "\033[00;34m$@\033[0m\n"
@@ -24,15 +25,23 @@ info() {
 cd "$SWIFT_SOURCE_ROOT" || exit
 
 info "Cloning repositories"
-git clone git@github.com:apple/swift-clang.git "$SWIFT_CLANG_ROOT"
-git clone git@github.com:apple/swift-cmark.git "$SWIFT_CMARK_ROOT"
-git clone git@github.com:apple/swift-lldb.git "$SWIFT_LLDB_ROOT"
-git clone git@github.com:apple/swift-llvm.git "$SWIFT_LLVM_ROOT"
-git clone git@github.com:apple/swift.git "$SWIFT_SWIFT_ROOT"
+
+git clone git@github.com:apple/swift-clang.git "$SWIFT_CLANG_ROOT" &> /dev/null &
+git clone git@github.com:apple/swift-cmark.git "$SWIFT_CMARK_ROOT" &> /dev/null &
+git clone git@github.com:apple/swift-lldb.git "$SWIFT_LLDB_ROOT" &> /dev/null &
+git clone git@github.com:apple/swift-llvm.git "$SWIFT_LLVM_ROOT" &> /dev/null &
+git clone git@github.com:apple/swift.git "$SWIFT_SWIFT_ROOT" &> /dev/null &
+
+git clone git@github.com:apple/swift-package-manager.git "$SWIFT_SWIFTPM_ROOT" &> /dev/null &
+
+git clone git@github.com:apple/swift-corelibs-foundation.git &> /dev/null &
+git clone git@github.com:apple/swift-corelibs-libdispatch.git &> /dev/null &
+git clone git@github.com:apple/swift-corelibs-xctest.git &> /dev/null &
+
+wait
 
 info "Updating checkout"
 "$SWIFT_SWIFT_ROOT/utils/update-checkout" --scheme=master --reset-to-remote
-
 
 if [ $# -ne 0 ]; then
   info "Invoking build script"
