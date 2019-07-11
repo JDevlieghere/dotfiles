448502c2366b76373f155e79a0c393542704133b
Jonas Devlieghere <jonas@devlieghere.com>
2019-07-11 09:17:44 -0700

    Update checkout-all to find submodules.

--- .config/fish/functions/checkout-all.fish
+++ .config/fish/functions/checkout-all.fish
@@ -1,6 +1,6 @@
 function checkout-all -d "Recursively checkout git repos"
   set -lx cwd (pwd)
-  for i in (find . -type d -name ".git")
+  for i in (find . -name ".git")
     cd (dirname "$i")
     git fetch --tags
     git checkout "$argv"
