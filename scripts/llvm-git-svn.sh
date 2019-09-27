321f686590b0a53517f049ec20a020883064b356
Jonas Devlieghere <jonas@devlieghere.com>
2019-09-27 14:37:43 -0700

    [llvm] Avoid redirect in git URLs

--- scripts/llvm-git-svn.sh
+++ scripts/llvm-git-svn.sh
@@ -25,34 +25,34 @@ function init {
 }
 
 # llvm
-git clone https://llvm.org/git/llvm.git
+git clone https://git.llvm.org/git/llvm.git
 init "$ROOT/llvm" "llvm"
 
 if [ -z "$1" ]; then
     # clang
     cd "$ROOT/llvm/tools" || exit
-    git clone https://llvm.org/git/clang.git
+    git clone https://git.llvm.org/git/clang.git
     init "$ROOT/llvm/tools/clang" "cfe"
 
     # clang-tools-extra
     cd "$ROOT/llvm/tools/clang/tools" || exit
-    git clone https://llvm.org/git/clang-tools-extra.git extra
+    git clone https://git.llvm.org/git/clang-tools-extra.git extra
     init "$ROOT/llvm/tools/clang/tools/extra" "clang-tools-extra"
 
     # lldb
     cd "$ROOT/llvm/tools" || exit
-    git clone https://llvm.org/git/lldb.git
+    git clone https://git.llvm.org/git/lldb.git
     init "$ROOT/llvm/tools/lldb" "lldb"
 
     # compiler-rt
     cd "$ROOT/llvm/projects" || exit
-    git clone https://llvm.org/git/compiler-rt.git
+    git clone https://git.llvm.org/git/compiler-rt.git
     init "$ROOT/llvm/projects/compiler-rt" "compiler-rt"
 
     # libcxx
     cd "$ROOT/llvm/projects" || exit
-    git clone http://llvm.org/git/libcxx.git
-    git clone http://llvm.org/git/libcxxabi.git
+    git clone https://git.llvm.org/git/libcxx.git
+    git clone https://git.llvm.org/git/libcxxabi.git
 fi
 
 # Create build and install dirs
