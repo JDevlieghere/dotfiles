a3b1df1892012be0fbb8c942cc163972d9f3e5cb
Jonas Devlieghere <jonas@devlieghere.com>
2019-10-24 14:42:47 -0700

    Only source some files in interactive mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,16 @@
 # No greeting.
 set fish_greeting
 
-# Source dotfiles.
-source ~/.aliases
-source ~/.exports
+if status --is-interactive
+  # Source dotfiles.
+  source ~/.aliases
+  source ~/.exports
 
-# Source fish files.
-source ~/.config/fish/gpg.fish
-source ~/.config/fish/fzf.fish
-source ~/.config/fish/ssh.fish
+  # Source fish files.
+  source ~/.config/fish/gpg.fish
+  source ~/.config/fish/fzf.fish
+  source ~/.config/fish/ssh.fish
 
-# Source localrc which is not sync'd.
-source ~/.localrc
+  # Source localrc which is not sync'd.
+  source ~/.localrc
+end
