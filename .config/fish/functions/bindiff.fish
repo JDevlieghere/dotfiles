b93c8b3fda14f7b403f951fe67942633ce2cad60
Jonas Devlieghere <jonas@devlieghere.com>
2020-01-14 12:14:58 -0800

    Fix bug in bindiff

--- .config/fish/functions/bindiff.fish
+++ .config/fish/functions/bindiff.fish
@@ -3,7 +3,7 @@ function bindiff -d "Diff binary files"
   set --local tmp2 (mktemp /tmp/XXXXXX)
 
   xxd -c 1 $argv[1] | cut -d ' ' -f 2 > $tmp1
-  xxd -c 1 $argv[1] | cut -d ' ' -f 2 > $tmp2
+  xxd -c 1 $argv[2] | cut -d ' ' -f 2 > $tmp2
   vimdiff $tmp1 $tmp2
 end
 
