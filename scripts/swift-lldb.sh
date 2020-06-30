9c0dc6885139646abe3b81e87de0e876049ec057
Jonas Devlieghere <jonas@devlieghere.com>
2020-06-30 09:24:04 -0700

    [Scripts] Remove swift.sh and update swift-lldb.sh

--- scripts/swift-lldb.sh
+++ scripts/swift-lldb.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-swift/utils/build-script \
-  --cmake-c-launcher `which sccache` \
-  --cmake-cxx-launcher `which sccache` \
+./swift/utils/build-script \
   --lldb \
   --release-debuginfo \
-  --test \
-  -- \
   --skip-build-benchmarks \
   --no-swift-stdlib-assertions \
   --lldb-use-system-debugserver \
   --skip-test-swift \
-  --skip-test-cmark $@
+  --skip-test-cmark \
+  $@
--- scripts/swift.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Clone the swift repository.
-git clone git@github.com:apple/swift.git
-
-# Use the update-checkout script to clone everyting else.
-python swift/utils/update-checkout --scheme master --clone-with-ssh
