8b6ada9e18582ad671e8bac6f1ccb26764609dd1
Jonas Devlieghere <jonas@devlieghere.com>
2020-09-18 15:12:56 -0700

    Add update-swift script

---
+++ scripts/update-swift.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Automatically updates and rebuilds Swift. It can be particularly convenient
+# for setting up a cron job.
+#
+# usage: update-swift.sh <scheme> <path>
+
+readonly scheme=$1
+readonly root=$2
+
+# Make this low priority.
+renice 19 -p $$ >/dev/null 2>&1
+ionice -c3 -p $$ >/dev/null 2>&1
+
+# Extend path for when running under cron.
+export PATH=/usr/local/bin:$PATH
+
+cd "$root" || exit
+
+./swift/utils/update-checkout \
+    --scheme "$scheme"
+
+./swift/utils/build-script \
+    --lldb \
+    --release-debuginfo \
+    --no-swift-stdlib-assertions \
+    --skip-build-benchmarks \
+    --skip-test-swift \
+    --skip-test-cmark
