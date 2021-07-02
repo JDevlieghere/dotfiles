4e88f737879f9d7826d400608044f0193de83f87
Jonas Devlieghere <jonas@devlieghere.com>
2021-07-02 15:25:19 -0700

    [gpg-agent] Adjust for --write-env-file removal

--- .config/fish/gpg.fish
+++ .config/fish/gpg.fish
@@ -1,25 +1,9 @@
 # GPG agent
-function __refresh_gpg_agent_info -d "Reload ~/.gpg-agent-info into environment"
-	cat ~/.gpg-agent-info | sed 's/=/ /' | while read key value
-		set -e $key
-		set -U -x $key "$value"
-	end
-end
-
-if command -s gpg-agent > /dev/null
-	if not set -q -x GPG_AGENT_INFO
-		gpg-agent --daemon --write-env-file ~/.gpg-agent-info > /dev/null ^ /dev/null
-	end
-
-	if test -f ~/.gpg-agent-info
-		__refresh_gpg_agent_info
-
-		gpg-connect-agent /bye ^/dev/null
-		if test $status -eq 1
-			pkill -U $USER gpg-agent
-			gpg-agent --daemon --write-env-file ~/.gpg-agent-info > /dev/null ^ /dev/null
-			__refresh_gpg_agent_info
-		end
+if command -s gpg-agent 2>&1 > /dev/null
+	gpg-connect-agent --quiet /bye 2> /dev/null
+	if test $status -eq 1
+		pkill -U $USER gpg-agent
+		gpg-agent --daemon 2>&1 > /dev/null
 	end
 
 	# Use curses based Pinentry
