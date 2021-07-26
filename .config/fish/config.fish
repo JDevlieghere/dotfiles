0573ea0fd1af9dc427092c01e14c78e71a43aecc
Jonas Devlieghere <jonas@devlieghere.com>
2021-07-26 15:19:21 -0700

    Source .shellenv first

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,11 @@ set fish_greeting
 set fish_emoji_width 2
 
 if status --is-interactive
+  # Source .shellenv first.
+  if test -f ~/.shellenv
+    source ~/.shellenv
+  end
+
   # Source dotfiles.
   source ~/.aliases
   source ~/.exports
@@ -13,6 +18,8 @@ if status --is-interactive
   source ~/.config/fish/fzf.fish
   source ~/.config/fish/ssh.fish
 
-  # Source localrc which is not sync'd.
-  source ~/.localrc
+  # Source .localrc last which is not sync'd.
+  if test -f ~/.shellenv
+    source ~/.localrc
+  end
 end
