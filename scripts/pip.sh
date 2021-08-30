5cf557d358b57115051aa7767abb3b65697961d0
Jonas Devlieghere <jonas@devlieghere.com>
2021-08-29 23:18:04 -0700

    [pip] Remove a few packages

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -12,11 +12,9 @@ sudo pip3 install --upgrade mkdocs
 sudo pip3 install --upgrade mkdocs-material
 sudo pip3 install --upgrade neovim
 sudo pip3 install --upgrade numpy
-sudo pip3 install --upgrade pandas
 sudo pip3 install --upgrade pyflakes
 sudo pip3 install --upgrade pylint
 sudo pip3 install --upgrade python-language-server
-sudo pip3 install --upgrade rtv
 sudo pip3 install --upgrade scipy
 sudo pip3 install --upgrade sphinx
 sudo pip3 install --upgrade yapf
