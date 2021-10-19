916eb6a41aea71367eb6012ce3f7dd5ea71aabc8
Jonas Devlieghere <jonas@devlieghere.com>
2021-10-18 21:24:54 -0700

    [pip] Cleanup packages

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -6,15 +6,9 @@ pip3 install --upgrade pip
 # Install packages
 pip3 install --upgrade autopep8
 pip3 install --upgrade gprof2dot
-pip3 install --upgrade haxor-news
-pip3 install --upgrade jedi
-pip3 install --upgrade mkdocs
-pip3 install --upgrade mkdocs-material
 pip3 install --upgrade neovim
-pip3 install --upgrade numpy
 pip3 install --upgrade pyflakes
 pip3 install --upgrade pylint
 pip3 install --upgrade python-language-server
-pip3 install --upgrade scipy
 pip3 install --upgrade sphinx
 pip3 install --upgrade yapf
