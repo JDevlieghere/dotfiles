0f94210d409000e84796d019f589ce0daa385085
Jonas Devlieghere <jonas@devlieghere.com>
2021-10-18 21:18:10 -0700

    [pip] Don't run as root

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Make sure we have the latest version
-sudo pip3 install --upgrade pip
+pip3 install --upgrade pip
 
 # Install packages
-sudo pip3 install --upgrade autopep8
-sudo pip3 install --upgrade gprof2dot
-sudo pip3 install --upgrade haxor-news
-sudo pip3 install --upgrade jedi
-sudo pip3 install --upgrade mkdocs
-sudo pip3 install --upgrade mkdocs-material
-sudo pip3 install --upgrade neovim
-sudo pip3 install --upgrade numpy
-sudo pip3 install --upgrade pyflakes
-sudo pip3 install --upgrade pylint
-sudo pip3 install --upgrade python-language-server
-sudo pip3 install --upgrade scipy
-sudo pip3 install --upgrade sphinx
-sudo pip3 install --upgrade yapf
+pip3 install --upgrade autopep8
+pip3 install --upgrade gprof2dot
+pip3 install --upgrade haxor-news
+pip3 install --upgrade jedi
+pip3 install --upgrade mkdocs
+pip3 install --upgrade mkdocs-material
+pip3 install --upgrade neovim
+pip3 install --upgrade numpy
+pip3 install --upgrade pyflakes
+pip3 install --upgrade pylint
+pip3 install --upgrade python-language-server
+pip3 install --upgrade scipy
+pip3 install --upgrade sphinx
+pip3 install --upgrade yapf
