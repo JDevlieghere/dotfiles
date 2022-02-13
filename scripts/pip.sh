91ea0e0c7ada83502236965628eb9f57ccf43aac
Jonas Devlieghere <jonas@devlieghere.com>
2022-02-12 22:41:31 -0800

    [pip.sh] Make python installation configurable

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+if [ "$#" -eq 0 ] ; then
+  python=python3
+else
+  python=$*
+fi
+
 # Make sure we have the latest version
-pip3 install --upgrade pip
+$python -m pip install --upgrade pip
 
 # Install packages
-pip3 install --upgrade autopep8
-pip3 install --upgrade gprof2dot
-pip3 install --upgrade neovim
-pip3 install --upgrade pyflakes
-pip3 install --upgrade pylint
-pip3 install --upgrade python-language-server
-pip3 install --upgrade rich
-pip3 install --upgrade sphinx
-pip3 install --upgrade yapf
+$python -m pip install --upgrade autopep8
+$python -m pip install --upgrade gprof2dot
+$python -m pip install --upgrade neovim
+$python -m pip install --upgrade pyflakes
+$python -m pip install --upgrade pylint
+$python -m pip install --upgrade python-language-server
+$python -m pip install --upgrade rich
+$python -m pip install --upgrade sphinx
+$python -m pip install --upgrade yapf
