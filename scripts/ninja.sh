9bbf72ee99c9554011376c04c3a85aeecc3015d4
Jonas Devlieghere <jonas@devlieghere.com>
2022-08-15 11:07:26 -0700

    Update scripts/ninja.sh

--- scripts/ninja.sh
+++ scripts/ninja.sh
@@ -3,12 +3,12 @@
 BUILD_DIR=/tmp/ninja
 
 # Clone
-git clone git://github.com/ninja-build/ninja.git "$BUILD_DIR"
+git clone https://github.com/ninja-build/ninja.git "$BUILD_DIR"
 cd "$BUILD_DIR" || exit 1
 git checkout release
 
 # Build
-./configure.py --bootstrap
+python3 ./configure.py --bootstrap
 
 # Install
 sudo cp ninja /usr/local/bin
