36fc7ea5c0e68a3580872a01f6185135a1c654a9
Jonas Devlieghere <jonas@devlieghere.com>
2022-08-15 13:08:58 -0700

    [fish] Check that brew exists before adding completions

--- .config/fish/brew.fish
+++ .config/fish/brew.fish
@@ -1,7 +1,9 @@
-if test -d (brew --prefix)"/share/fish/completions"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-end
+if type -q brew
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+    end
 
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
 end
