6641ed00c651f10f9b47193f609fa1bc377096d6
Jonas Devlieghere <jonas@devlieghere.com>
2022-08-15 12:12:41 -0700

    Use gold on linux

--- os/linux.sh
+++ os/linux.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Use gold
+sudo unlink /usr/bin/ld && sudo ln -s /usr/bin/ld.gold /usr/bin/ld
+
 if [ "$GDMSESSION" == "gnome" ];  then
   echo "Configuring GNOME"
   gsettings set org.gnome.desktop.peripherals.keyboard delay 250
   gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15
 fi
+
