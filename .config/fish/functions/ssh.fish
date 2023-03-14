b4919896538c260c6296d93e0d0e034045ba3dbe
Jonas Devlieghere <jonas@devlieghere.com>
2023-03-14 10:52:54 -0700

    [fish] Fix argv expansion

--- .config/fish/functions/ssh.fish
+++ .config/fish/functions/ssh.fish
@@ -1,9 +1,9 @@
 function ssh
   if test -n $TMUX
     tmux rename-window (echo "$argv" | cut -d '@' -f 2)
-    command ssh "$argv"
+    command ssh $argv
     tmux set-window-option automatic-rename "on" 1>/dev/null
   else
-    command ssh "$argv"
+    command ssh $argv
   end
 end
