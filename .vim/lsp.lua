42b2842cf911dfa9ef46fac37a7a073d1c165b8c
Jonas Devlieghere <jonas@devlieghere.com>
2023-05-04 21:37:40 -0700

    [vim] Map S-Tab to select_prev_item

--- .vim/lsp.lua
+++ .vim/lsp.lua
@@ -8,6 +8,13 @@ cmp.setup {
       else
         fallback()
       end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
     end
   },
   sources = {
