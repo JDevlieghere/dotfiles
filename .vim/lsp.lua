61ea7c65a356c9d8546a44ad6f26c8185595b551
Jonas Devlieghere <jonas@devlieghere.com>
2024-03-28 21:10:06 -0700

    Disable sourcekit-lsp

--- .vim/lsp.lua
+++ .vim/lsp.lua
@@ -33,10 +33,6 @@ lspconfig.clangd.setup {
   capabilities = capabilities,
 }
 
-lspconfig.sourcekit.setup {
-  capabilities = capabilities,
-}
-
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
