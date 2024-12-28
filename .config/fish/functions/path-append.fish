981f692fdd741b723c801cdd6d784af8c0389e86
Jonas Devlieghere <jonas@devlieghere.com>
2024-12-28 10:16:55 -0800

    Remove path-append & path-prepend

--- .config/fish/functions/path-append.fish
+++
@@ -1,7 +0,0 @@
-function path-append
-  if count $argv > /dev/null
-    set PATH $PATH $argv
-  else
-    set PATH $PATH $PWD
-  end
-end
--- .config/fish/functions/path-prepend.fish
+++
@@ -1,7 +0,0 @@
-function path-prepend
-  if count $argv > /dev/null
-    set PATH $argv $PATH
-  else
-    set PATH $PWD $PATH
-  end
-end
