cd985e2137c21d3485fa7110205ddce329fa1bb0
Jonas Devlieghere <jonas@devlieghere.com>
2024-12-28 10:16:19 -0800

    Prepend Homebrew's SWIG to PATH

--- .config/fish/brew.fish
+++ .config/fish/brew.fish
@@ -6,4 +6,8 @@ if type -q brew
     if test -d (brew --prefix)"/share/fish/vendor_completions.d"
         set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
     end
+
+    if test -d (brew --prefix swig)"/bin"
+        fish_add_path -p (brew --prefix swig)/bin
+    end
 end
