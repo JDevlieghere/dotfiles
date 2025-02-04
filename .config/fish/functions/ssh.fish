4536f6d83a0920ef41e9260f62a4b5f90a7bc456
Jonas Devlieghere <jonas@devlieghere.com>
2025-02-04 09:42:54 -0800

    Add missing quotes

--- .config/fish/functions/ssh.fish
+++ .config/fish/functions/ssh.fish
@@ -6,7 +6,7 @@ function ssh
     set -f ssh_command "ssh"
   end
 
-  if test -n $TMUX
+  if test -n "$TMUX"
     tmux rename-window (echo "$argv" | cut -d '@' -f 2)
     command $ssh_command $ssh_options $argv
     tmux set-window-option automatic-rename "on" 1>/dev/null
