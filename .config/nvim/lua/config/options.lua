7a8ded6fc2ab8105850c268abff843634647b5f2
Jonas Devlieghere <jonas@devlieghere.com>
2025-02-16 10:48:24 -0800

    [nvim] Use diagonal lines for deleted lines in diff-mode.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,17 @@
+-- Enable 24-bit RGB color in the TUI.
 vim.o.termguicolors = true
+
+-- Assume a dark terminal background.
 vim.o.background = "dark"
+
+-- Shows the effects of a command incrementally in the buffer and off-screen
+-- results in a preview window.
 vim.o.inccommand = "split"
 
+-- Use diagonal lines for deleted lines in diff-mode.
+vim.opt.fillchars:append { diff = "╱" }
+
+-- Change leader to space.
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
