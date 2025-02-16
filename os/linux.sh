406ace8890d6870b87259a550ae738b2d929a474
Jonas Devlieghere <jonas@devlieghere.com>
2025-02-16 18:23:53 +0000

    [linux] Use LLD and generate locale

--- os/linux.sh
+++ os/linux.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-# Use gold
-sudo unlink /usr/bin/ld && sudo ln -s /usr/bin/ld.gold /usr/bin/ld
+# Generate locale
+sudo locale-gen "en_US.UTF-8"
+sudo dpkg-reconfigure locales
 
+# Use LLD if available.
+lld=$(which lld)
+if [ -x "$lld" ]; then
+  sudo unlink /usr/bin/ld && sudo ln -s "$lld" /usr/bin/ld
+fi
+
+# Configure keyboard repeat in GNOME.
 if [ "$GDMSESSION" == "gnome" ];  then
   echo "Configuring GNOME"
   gsettings set org.gnome.desktop.peripherals.keyboard delay 250
