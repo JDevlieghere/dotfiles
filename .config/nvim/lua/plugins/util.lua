f16d65a41104fa559125a955a1ccc05b0f67522e
Jonas Devlieghere <jonas@devlieghere.com>
2025-02-17 22:20:31 -0800

    Drop persistence.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -1,14 +1,5 @@
 return {
     {
-        "folke/persistence.nvim",
-        event = "BufReadPre", -- this will only start session saving when an actual file was opened
-        opts = {
-            dir = vim.fn.stdpath("state") .. "/sessions/", -- directory where session files are saved
-            need = 1, -- minimum number of file buffers that need to be open to save
-            branch = true, -- use git branch to save session
-        },
-    },
-    {
         "folke/which-key.nvim",
         event = "VeryLazy",
         opts = {},
