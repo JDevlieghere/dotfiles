938a73d4e0b4948a4e7224ea01c2853dd41c4cda
Jonas Devlieghere <jonas@devlieghere.com>
2025-09-18 15:17:18 -0700

    Migrate to new LSP APIs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -16,12 +16,12 @@ return {
             },
         },
         config = function(_, opts)
-            local lspconfig = require("lspconfig")
             for server, config in pairs(opts.servers) do
                 -- passing config.capabilities to blink.cmp merges with the capabilities in your
                 -- `opts[server].capabilities, if you've defined it
                 config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
+                vim.lsp.config[server] = config
+                vim.lsp.enable(server)
             end
         end,
     },
